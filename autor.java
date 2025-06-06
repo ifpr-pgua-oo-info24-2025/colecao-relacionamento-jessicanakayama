10c27386653b91c5e7017c0399734defb7da5701
jessica <jessicaynakayama@gmail.com>
2025-06-05 21:21:01 -0300

    autor.java

---
+++ autor.java
@@ -0,0 +1,20 @@
+public class Autor {
+    private String nome;
+    private String nacionalidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+}
